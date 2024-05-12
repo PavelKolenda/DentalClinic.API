d52795731ab43fd1604e686ea48100c4040aba78
Pavel-Kolenda <progap0002@gmail.com>
2024-05-12 14:49:46 +0300

    Add PagedList

---
+++ DentalClinic.Shared/Pagination/PagedListExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace DentalClinic.Shared.Pagination;
+
+public static class PagedListExtensions<T>
+{
+    public static PagedList<T> Create(IQueryable<T> source, int page, int pageSize)
+    {
+        int totalCount = source.Count();
+
+        var items = source
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+
+        return new PagedList<T>(items, page, pageSize, totalCount);
+    }
+}
