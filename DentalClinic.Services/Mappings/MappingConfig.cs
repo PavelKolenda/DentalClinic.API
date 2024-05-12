c2c2d660d51af5604704ad9f8ce8b22be0f37a53
Pavel-Kolenda <progap0002@gmail.com>
2024-05-12 14:58:01 +0300

    Add MappingConfig

---
+++ DentalClinic.Services/Mappings/MappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using DentalClinic.Models.Entities;
+using DentalClinic.Shared.DTOs.Dentists;
+using DentalClinic.Shared.DTOs.Patients;
+
+using Mapster;
+
+namespace DentalClinic.Services.Mappings;
+public class MappingConfig
+{
+    public static void Configure()
+    {
+        TypeAdapterConfig<PatientCreateDto, Patient>.NewConfig()
+            .Map(dest => dest.PasswordHash, src => src.Password);
+
+        TypeAdapterConfig<Patient, PatientCreateDto>.NewConfig();
+
+        TypeAdapterConfig<Dentist, DentistDto>.NewConfig()
+            .Map(dest => dest.Specialization, src => src.Specialization.Name);
+        TypeAdapterConfig<DentistDtoBase, Dentist>.NewConfig()
+            .Ignore(dest => dest.Specialization);
+    }
+}
