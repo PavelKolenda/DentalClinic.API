c3989c6c589c0cc31a20fa16b53a6dea502e7210
Pavel-Kolenda <progap0002@gmail.com>
2024-05-12 15:14:50 +0300

    Add Tests for Dentist validators

---
+++ DentalClinic.Tests/UnitTesting/Validators/DentistValidatorsTests.cs
@@ -0,0 +1,91 @@
+﻿using DentalClinic.API.Validators.Dentists;
+using DentalClinic.Shared.DTOs.Dentists;
+
+using FluentValidation.TestHelper;
+
+namespace DentalClinic.Tests.UnitTesting.Validators;
+
+public class DentistValidatorsTests
+{
+    public DentistValidatorsTests()
+    {
+    }
+
+    [Fact]
+    public void DentistCreateDto_WhenPropertiesInvalid_ShouldThrowException()
+    {
+        DentistCreateDto dentist = new()
+        {
+            Name = "",
+            Surname = "",
+            Patronymic = "",
+            CabinetNumber = -12,
+            Specialization = ""
+        };
+
+        DentistCreateDtoValidator validator = new();
+        var validationResult = validator.TestValidate(dentist);
+
+        validationResult.ShouldHaveValidationErrorFor(x => x.Name);
+        validationResult.ShouldHaveValidationErrorFor(x => x.Surname);
+        validationResult.ShouldHaveValidationErrorFor(x => x.CabinetNumber);
+        validationResult.ShouldHaveValidationErrorFor(x => x.Specialization);
+    }
+
+    [Fact]
+    public void DentistCreateDto_WhenPropertiesValid_ShouldPass()
+    {
+        DentistCreateDto dentist = new()
+        {
+            Name = "test",
+            Surname = "test",
+            Patronymic = "test",
+            CabinetNumber = 10,
+            Specialization = "test"
+        };
+
+        DentistCreateDtoValidator validator = new();
+        var validationResult = validator.TestValidate(dentist);
+
+        validationResult.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public void DentistUpdateDto_WhenPropertiesInvalid_ShouldThrowExceptions()
+    {
+        DentistUpdateDto dentist = new()
+        {
+            Name = "",
+            Surname = "",
+            Patronymic = "",
+            CabinetNumber = -12,
+            Specialization = ""
+        };
+
+        DentistUpdateDtoValidator validator = new();
+        var validationResult = validator.TestValidate(dentist);
+
+        validationResult.ShouldHaveValidationErrorFor(x => x.Name);
+        validationResult.ShouldHaveValidationErrorFor(x => x.Surname);
+        validationResult.ShouldHaveValidationErrorFor(x => x.CabinetNumber);
+        validationResult.ShouldHaveValidationErrorFor(x => x.Specialization);
+    }
+
+    [Fact]
+    public void DentistUpdateDto_WhenPropertiesValid_ShouldPass()
+    {
+        DentistUpdateDto dentist = new()
+        {
+            Name = "test",
+            Surname = "test",
+            Patronymic = "test",
+            CabinetNumber = 10,
+            Specialization = "test"
+        };
+
+        DentistUpdateDtoValidator validator = new();
+        var validationResult = validator.TestValidate(dentist);
+
+        validationResult.ShouldNotHaveAnyValidationErrors();
+    }
+}
