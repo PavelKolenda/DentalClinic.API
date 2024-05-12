ecd21e00c4c739efb9ec6f017ca1f0878772f068
Pavel-Kolenda <progap0002@gmail.com>
2024-05-12 14:53:52 +0300

    Add Dentists DTO's

---
+++ DentalClinic.Shared/DTOs/Dentists/DentistDto.cs
@@ -0,0 +1,6 @@
+﻿namespace DentalClinic.Shared.DTOs.Dentists;
+
+public class DentistDto : DentistDtoBase
+{
+    public int Id { get; set; }
+}
---
+++ DentalClinic.Shared/DTOs/Dentists/DentistDtoBase.cs
@@ -0,0 +1,10 @@
+﻿namespace DentalClinic.Shared.DTOs.Dentists;
+
+public abstract class DentistDtoBase
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string? Patronymic { get; set; }
+    public int CabinetNumber { get; set; }
+    public string Specialization { get; set; }
+}
