ec46c955f40584dd193349640082f33b347ec44a
Pavel-Kolenda <progap0002@gmail.com>
2024-05-12 15:01:23 +0300

    Add DentistCreateDto and DentistUpdateDto validators

---
+++ DentalClinic.API/Validators/Dentists/DentistCreateDtoValidator.cs
@@ -0,0 +1,27 @@
+﻿using DentalClinic.Shared.DTOs.Dentists;
+
+using FluentValidation;
+
+namespace DentalClinic.API.Validators.Dentists;
+public class DentistCreateDtoValidator : AbstractValidator<DentistCreateDto>
+{
+    public DentistCreateDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .MinimumLength(2)
+            .MaximumLength(75)
+            .NotEmpty();
+
+        RuleFor(x => x.Surname)
+            .MinimumLength(2)
+            .MaximumLength(75)
+            .NotEmpty();
+
+        RuleFor(x => x.CabinetNumber)
+            .InclusiveBetween(1, 25)
+            .NotEmpty();
+
+        RuleFor(x => x.Specialization)
+            .NotEmpty();
+    }
+}
