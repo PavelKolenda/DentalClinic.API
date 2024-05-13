6bb36f3615bd056b361ca5983ba6e7609ecdbb28
Pavel-Kolenda <progap0002@gmail.com>
2024-05-13 16:16:23 +0300

    Add PatientCreateDto and PatientLoginDto

---
+++ DentalClinic.Shared/DTOs/Patients/PatientCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DentalClinic.Shared.DTOs.Patients;
+public class PatientCreateDto
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string? Patronymic { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public DateOnly BirthDate { get; set; }
+}
