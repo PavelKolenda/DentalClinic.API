93efd100d13a19e04f0cb470b82c03eae9c03374
Pavel-Kolenda <progap0002@gmail.com>
2024-05-13 17:08:18 +0300

    Add PatientCreateDto and PatientLoginDto validators

---
+++ DentalClinic.API/Validators/Patients/PatientCreateDtoValidator.cs
@@ -0,0 +1,32 @@
+﻿using DentalClinic.Shared.DTOs.Patients;
+
+using FluentValidation;
+
+namespace DentalClinic.API.Validators.Patients;
+public class PatientCreateDtoValidator : AbstractValidator<PatientCreateDto>
+{
+    public PatientCreateDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(75)
+            .MinimumLength(2);
+
+        RuleFor(x => x.Surname)
+            .NotEmpty()
+            .MaximumLength(75)
+            .MinimumLength(2);
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(8)
+            .MaximumLength(128);
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.BirthDate)
+            .NotEmpty();
+    }
+}
---
+++ DentalClinic.API/Validators/Patients/PatientLoginDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using DentalClinic.Shared.DTOs.Patients;
+
+using FluentValidation;
+
+namespace DentalClinic.API.Validators.Patients;
+
+public class PatientLoginDtoValidator : AbstractValidator<PatientLoginDto>
+{
+    public PatientLoginDtoValidator()
+    {
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(8)
+            .MaximumLength(128);
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+    }
+}
