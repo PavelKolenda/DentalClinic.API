93537ead78db7c64a29a7e961e5cb0c7d81f6092
Pavel-Kolenda <progap0002@gmail.com>
2024-05-13 17:11:13 +0300

    Add PatientValidatorsTests

---
+++ DentalClinic.Tests/UnitTesting/Validators/PatientValidatorsTests.cs
@@ -0,0 +1,78 @@
+﻿using DentalClinic.API.Validators.Patients;
+using DentalClinic.Shared.DTOs.Patients;
+
+using FluentValidation.TestHelper;
+
+namespace DentalClinic.Tests.UnitTesting.Validators;
+public class PatientValidatorsTests
+{
+    [Fact]
+    public void PatientCreateDto_WhenPropertiesInvalid_ShouldThrowException()
+    {
+        PatientCreateDto patient = new()
+        {
+            Name = "",
+            Surname = "",
+            Patronymic = "",
+            BirthDate = new DateOnly(1999, 1, 1),
+            Email = "",
+            Password = ""
+        };
+
+        PatientCreateDtoValidator validator = new();
+        var validationResult = validator.TestValidate(patient);
+
+        validationResult.ShouldHaveValidationErrorFor(x => x.Name);
+        validationResult.ShouldHaveValidationErrorFor(x => x.Surname);
+        validationResult.ShouldHaveValidationErrorFor(x => x.Email);
+        validationResult.ShouldHaveValidationErrorFor(x => x.Password);
+    }
+
+    [Fact]
+    public void PatientCreateDto_WhenPropertiesValid_ShouldPass()
+    {
+        PatientCreateDto patient = new()
+        {
+            Name = "Тест",
+            Surname = "Тест",
+            Patronymic = "Тест",
+            BirthDate = new DateOnly(2012, 12, 12),
+            Email = "test@gmail.com",
+            Password = "testtesttest"
+        };
+
+        PatientCreateDtoValidator validator = new();
+        var validationResult = validator.TestValidate(patient);
+        validationResult.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public void PatientLoginDto_WhenPropertiesInvalid_ShouldThrowException()
+    {
+        PatientLoginDto patient = new()
+        {
+            Email = "f@",
+            Password = "12345"
+        };
+
+        PatientLoginDtoValidator validator = new();
+        var validationResult = validator.TestValidate(patient);
+
+        validationResult.ShouldHaveValidationErrorFor(x => x.Email);
+        validationResult.ShouldHaveValidationErrorFor(x => x.Password);
+    }
+
+    [Fact]
+    public void PatientLoginDto_WhenPropertiesValid_ShouldPass()
+    {
+        PatientLoginDto patient = new()
+        {
+            Email = "realemail@gmail.com",
+            Password = "strongPassword"
+        };
+
+        PatientLoginDtoValidator validator = new();
+        var validationResult = validator.TestValidate(patient);
+        validationResult.ShouldNotHaveAnyValidationErrors();
+    }
+}
