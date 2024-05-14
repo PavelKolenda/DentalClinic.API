ffa69e5d6c48e2b392ce4f7020b90c0a8d6b07db
Pavel-Kolenda <progap0002@gmail.com>
2024-05-14 10:48:09 +0300

    Add ValidateIdFilter

---
+++ DentalClinic.API/Filters/ValidateIdAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DentalClinic.API.Filters;
+public class ValidateIdAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        if (context.ActionArguments.TryGetValue("id", out var idObj) && idObj is int id)
+        {
+            if (id < 0 || id > int.MaxValue)
+            {
+                context.Result = new BadRequestObjectResult("Invalid Id");
+            }
+        }
+
+        base.OnActionExecuting(context);
+    }
+}
