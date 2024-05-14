caa930da029be0aa98af788d83279a3a34eaf7bd
Pavel-Kolenda <progap0002@gmail.com>
2024-05-14 10:42:51 +0300

    Add PatientDto and PatientUpdateDto

---
+++ DentalClinic.Shared/DTOs/Patients/PatientDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DentalClinic.Shared.DTOs.Patients;
+
+public class PatientDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string? Patronymic { get; set; }
+    public string Email { get; set; }
+    public DateOnly BirthDate { get; set; }
+}
---
+++ DentalClinic.Shared/DTOs/Patients/PatientUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DentalClinic.Shared.DTOs.Patients;
+
+public class PatientUpdateDto
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string? Patronymic { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public DateOnly BirthDate { get; set; }
+}
