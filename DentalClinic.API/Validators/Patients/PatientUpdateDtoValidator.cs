d34bacac6b0c74d5c73e60cda0e0b610bfa15ebc
Pavel-Kolenda <progap0002@gmail.com>
2024-05-14 10:52:06 +0300

    Add PatientUpdateDtoValidator

---
+++ DentalClinic.API/Validators/Patients/PatientUpdateDtoValidator.cs
@@ -0,0 +1,33 @@
+﻿using DentalClinic.Shared.DTOs.Patients;
+
+using FluentValidation;
+
+namespace DentalClinic.API.Validators.Patients;
+
+public class PatientUpdateDtoValidator : AbstractValidator<PatientUpdateDto>
+{
+    public PatientUpdateDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(75)
+            .MinimumLength(2);
+
+        RuleFor(x => x.Surname)
+            .NotEmpty()
+            .MaximumLength(75)
+            .MinimumLength(2);
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(8)
+            .MaximumLength(128);
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.BirthDate)
+            .NotEmpty();
+    }
+}
