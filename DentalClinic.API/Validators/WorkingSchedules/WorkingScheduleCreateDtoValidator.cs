de1217fb2846830d084291cfb53b3dc427d7c7e7
Pavel-Kolenda <progap0002@gmail.com>
2024-05-15 23:00:04 +0300

    Add WorkingScheduleCreate and Update dto validators

---
+++ DentalClinic.API/Validators/WorkingSchedules/WorkingScheduleCreateDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using DentalClinic.Shared.DTOs.WorkingSchedules;
+
+using FluentValidation;
+
+namespace DentalClinic.API.Validators.WorkingSchedules;
+public class WorkingScheduleCreateDtoValidator : AbstractValidator<WorkingScheduleCreateDto>
+{
+    public WorkingScheduleCreateDtoValidator()
+    {
+        RuleFor(x => x.Start)
+            .NotEmpty()
+            .LessThan(x => x.End);
+
+        RuleFor(x => x.End)
+            .NotEmpty()
+            .GreaterThan(x => x.Start);
+
+        RuleFor(x => x.WorkingDay)
+            .NotEmpty();
+    }
+}
---
+++ DentalClinic.API/Validators/WorkingSchedules/WorkingScheduleUpdateDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using DentalClinic.Shared.DTOs.WorkingSchedules;
+
+using FluentValidation;
+
+namespace DentalClinic.API.Validators.WorkingSchedules;
+
+public class WorkingScheduleUpdateDtoValidator : AbstractValidator<WorkingScheduleUpdateDto>
+{
+    public WorkingScheduleUpdateDtoValidator()
+    {
+        RuleFor(x => x.Start)
+            .NotEmpty()
+            .LessThan(x => x.End);
+
+        RuleFor(x => x.End)
+            .NotEmpty()
+            .GreaterThan(x => x.Start);
+
+        RuleFor(x => x.WorkingDay)
+            .NotEmpty();
+    }
+}
