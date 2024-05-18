279f55ddbe53826990c113cc36a0c2d88391d031
Pavel-Kolenda <progap0002@gmail.com>
2024-05-18 15:25:52 +0300

    Add GlobalExceptiosHandling

---
+++ DentalClinic.API/Extensions/ErrorHandling/GlobalExceptionsHandling.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DentalClinic.API.Extensions.ErrorHandling;
+public class GlobalExceptionsHandling : IExceptionHandler
+{
+    public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        httpContext.Response.WriteAsJsonAsync(new ProblemDetails()
+        {
+            Status = httpContext.Response.StatusCode,
+            Detail = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1",
+        }, cancellationToken);
+
+        return ValueTask.FromResult(true);
+    }
+}
