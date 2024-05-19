f936522dfac28b6010e8c68b3349c68199d9e083
Pavel-Kolenda <progap0002@gmail.com>
2024-05-19 14:38:48 +0300

    Create migration

---
+++ DentalClinic.Repository/Migrations/20240519113206_MakePatinetIdInAppointmentsNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DentalClinic.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakePatientIdInAppointmentsNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PatientId",
+                table: "Appointments",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PatientId",
+                table: "Appointments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
