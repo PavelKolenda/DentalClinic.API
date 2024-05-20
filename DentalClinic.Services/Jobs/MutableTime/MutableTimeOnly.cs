2a898050d84f659e09b2f1df712e985e02ed0d5c
Pavel-Kolenda <progap0002@gmail.com>
2024-05-20 15:59:36 +0300

    Add mutable dataTime and dateOnly

---
+++ DentalClinic.Services/Jobs/MutableTime/MutableTimeOnly.cs
@@ -0,0 +1,21 @@
+﻿namespace DentalClinic.Services.Jobs.MutableTime;
+
+public class MutableTimeOnly
+{
+    public TimeOnly Time { get; private set; }
+
+    public MutableTimeOnly(TimeOnly time)
+    {
+        Time = time;
+    }
+
+    public void AddMinutes(int minutes)
+    {
+        Time = Time.AddMinutes(minutes);
+    }
+
+    public void AddHours(int hours)
+    {
+        Time = Time.AddHours(hours);
+    }
+}
