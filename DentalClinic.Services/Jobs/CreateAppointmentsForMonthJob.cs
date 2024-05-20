51a659c9dd06f60efb9600ff64ea6b577f6caf75
Pavel-Kolenda <progap0002@gmail.com>
2024-05-20 16:04:43 +0300

    Add CreateAppointmentsForMonthJob

---
+++ DentalClinic.Services/Jobs/CreateAppointmentsForMonthJob.cs
@@ -0,0 +1,83 @@
+﻿using DentalClinic.Models.Entities;
+using DentalClinic.Repository.Contracts;
+using DentalClinic.Services.Options;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using Quartz;
+
+namespace DentalClinic.Services.Jobs;
+
+[DisallowConcurrentExecution]
+public class CreateAppointmentsForMonthJob : CreateAppointmentsJobBase, IJob
+{
+    private readonly IAppointmentsRepository _appointmentsRepository;
+    private readonly IDentistRepository _dentistRepository;
+    private readonly ILogger<CreateDailyAppointmentsJob> _logger;
+    private readonly CreateAppointmentsOptions _options;
+
+    public CreateAppointmentsForMonthJob(IAppointmentsRepository appointmentsRepository,
+                                            ILogger<CreateDailyAppointmentsJob> logger,
+                                            IOptions<CreateAppointmentsOptions> options,
+                                            IDentistRepository dentistRepository)
+    {
+        _appointmentsRepository = appointmentsRepository;
+        _logger = logger;
+        _options = options.Value;
+        _dentistRepository = dentistRepository;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        var today = DateTime.UtcNow;
+
+        List<Appointment> appointments = [];
+
+        for (int i = 0; i < 31; i++)
+        {
+            var dayOfWeek = GetDayOfWeekAsString(today.DayOfWeek);
+
+            var dentists = await _dentistRepository
+                .GetAll()
+                .Include(ws => ws.WorkingSchedule)
+                .Where(d => d.WorkingSchedule.Any(ws => ws.WorkingDay == dayOfWeek))
+                 .Select(d => new
+                 {
+                     d.Id,
+                     WorkingSchedule = d.WorkingSchedule.FirstOrDefault(ws => ws.WorkingDay == dayOfWeek)
+                 })
+                .ToListAsync();
+
+            foreach (var dentist in dentists)
+            {
+                var workingSchedule = dentist.WorkingSchedule;
+
+                if (workingSchedule == null)
+                {
+                    _logger.LogWarning("");
+                    return;
+                }
+
+                int workingDayInMinutes = Convert.ToInt32((workingSchedule.End - workingSchedule.Start).TotalMinutes);
+
+                if (workingDayInMinutes < 0)
+                {
+                    _logger.LogWarning("");
+                    return;
+                }
+
+                int appointmentsCount = workingDayInMinutes / _options.TimeToOneAppointment.Minute;
+
+                appointments.AddRange(CreateAppointments(dentist.Id,
+                                                         workingSchedule.Start,
+                                                         DateTime.UtcNow.AddDays(i),
+                                                         appointmentsCount,
+                                                         _options.TimeToOneAppointment.Minute));
+            }
+        }
+
+        await _appointmentsRepository.CreateAppointments(appointments);
+    }
+}
