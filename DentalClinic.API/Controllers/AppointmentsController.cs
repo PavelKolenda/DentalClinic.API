e27bebc01b98c380a2425e7095f6a06868b8a30b
Pavel-Kolenda <progap0002@gmail.com>
2024-05-21 11:41:48 +0300

    Add AppointmentsController

---
+++ DentalClinic.API/Controllers/AppointmentsController.cs
@@ -0,0 +1,56 @@
+﻿using DentalClinic.API.Filters;
+using DentalClinic.Services.Contracts;
+using DentalClinic.Shared.DTOs;
+using DentalClinic.Shared.DTOs.Appointments;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DentalClinic.API.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+[ValidateId]
+public class AppointmentsController : ControllerBase
+{
+    private readonly IAppointmentsService _appointmentsService;
+    public AppointmentsController(IAppointmentsService appointmentsService)
+    {
+        _appointmentsService = appointmentsService;
+    }
+
+    [HttpGet("{dentistId:int}")]
+    [Authorize(Roles = "Patient")]
+    public async Task<ActionResult<IEnumerable<AvailableAppointmentsDto>>> GetAvailableForMonth(int dentistId)
+    {
+        var appointments = await _appointmentsService.GetAvailableForMonthAsync(dentistId);
+
+        return Ok(appointments);
+    }
+
+    [HttpPost("{dentistId:int}/{appointmentId:int}")]
+    [Authorize(Roles = "Patient")]
+    public async Task<ActionResult<AppointmentDto>> MakeAppointment(int dentistId, int appointmentId)
+    {
+        var appointment = await _appointmentsService.MakeAppointmentAsync(dentistId, appointmentId);
+
+        return Ok(appointment);
+    }
+
+    [HttpGet("{appointmentId:int}/get-appointment")]
+    [Authorize(Roles = "Patient")]
+    public async Task<ActionResult<AppointmentDto>> GetAppointment(int appointmentId)
+    {
+        var appointment = await _appointmentsService.GetById(appointmentId);
+
+        return Ok(appointment);
+    }
+
+    [HttpDelete("{appointmentId:int}/cancel-appointment")]
+    [Authorize(Roles = "Patient")]
+    public async Task<ActionResult> CancelAppointment(int appointmentId)
+    {
+        await _appointmentsService.CancelAppointmentAsync(appointmentId);
+
+        return NoContent();
+    }
+}
