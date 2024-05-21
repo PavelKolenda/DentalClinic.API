717a72953748fc2ba50942f818b9f09d385873fb
Pavel-Kolenda <progap0002@gmail.com>
2024-05-21 17:01:04 +0300

    Add Specializations DTO's validators

---
+++ DentalClinic.API/Validators/Specializations/SpecializationUpdateDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using DentalClinic.Shared.DTOs.Specializations;
+
+using FluentValidation;
+
+namespace DentalClinic.API.Validators.Specializations;
+
+public class SpecializationUpdateDtoValidator : AbstractValidator<SpecializationUpdateDto>
+{
+    public SpecializationUpdateDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .NotNull()
+            .MinimumLength(3)
+            .MaximumLength(128);
+    }
+}
