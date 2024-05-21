d10b73f139b4ba999360accc8b8b01dde6bfcd07
Pavel-Kolenda <progap0002@gmail.com>
2024-05-21 18:36:23 +0300

    Add SpecializationsController

---
+++ DentalClinic.API/Controllers/SpecializationsController.cs
@@ -0,0 +1,64 @@
+﻿using DentalClinic.API.Filters;
+using DentalClinic.Repository.Contracts.Queries;
+using DentalClinic.Services.Contracts;
+using DentalClinic.Shared.DTOs.Specializations;
+using DentalClinic.Shared.Pagination;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DentalClinic.API.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+[ValidateId]
+[Authorize(Roles = "Admin")]
+public class SpecializationsController : ControllerBase
+{
+    private readonly ISpecializationsService _specializationsService;
+
+    public SpecializationsController(ISpecializationsService specializationsService)
+    {
+        _specializationsService = specializationsService;
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public ActionResult<PagedList<SpecializationDto>> GetPaged([FromQuery] QueryParameters query)
+    {
+        var specializations = _specializationsService.GetPaged(query);
+
+        return Ok(specializations);
+    }
+
+    [HttpGet("{id:int}", Name = "GetSpecializationById")]
+    public async Task<ActionResult<SpecializationDto>> GetById(int id)
+    {
+        var specialization = await _specializationsService.GetByIdAsync(id);
+
+        return Ok(specialization);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<SpecializationDto>> CreateAsync([FromBody] SpecializationCreateDto specializationCreateDto)
+    {
+        var specialization = await _specializationsService.CreateAsync(specializationCreateDto);
+
+        return CreatedAtRoute(routeName: "GetSpecializationById", routeValues: new { id = specialization.Id }, value: specialization);
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdateAsync(int id, [FromBody] SpecializationUpdateDto specializationUpdateDto)
+    {
+        await _specializationsService.UpdateAsync(id, specializationUpdateDto);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteAsync(int id)
+    {
+        await _specializationsService.DeleteAsync(id);
+
+        return NoContent();
+    }
+}
