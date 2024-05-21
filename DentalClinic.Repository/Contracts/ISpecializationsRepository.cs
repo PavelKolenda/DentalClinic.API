4bab61a6d3f3c2a8cf8630be4da10f79393eb33b
Pavel-Kolenda <progap0002@gmail.com>
2024-05-21 16:59:31 +0300

    Rewrite SpecializationsRepository

--- DentalClinic.Repository/Contracts/ISpecializationsRepository.cs
+++ DentalClinic.Repository/Contracts/ISpecializationsRepository.cs
@@ -1,8 +1,15 @@
 ﻿using DentalClinic.Models.Entities;
+using DentalClinic.Repository.Contracts.Queries;
+using DentalClinic.Shared.Pagination;
 
 namespace DentalClinic.Repository.Contracts;
 public interface ISpecializationsRepository
 {
+    Task<Specialization> CreateAsync(Specialization specialization);
+    Task DeleteAsync(int id);
     IQueryable<Specialization> GetAll();
+    Task<Specialization> GetByIdAsync(int id, bool trackChanges);
     Task<Specialization> GetByName(string name);
+    PagedList<Specialization> GetPaged(QueryParameters query);
+    Task UpdateAsync(int id, Specialization specialization);
 }
