55d2f4b9141d3fdc0b605d8444bb272cdfffaf2f
Pavel-Kolenda <progap0002@gmail.com>
2024-05-27 22:31:47 +0300

    Add AuthResponse

---
+++ DentalClinic.Services/Auth/AuthResponse.cs
@@ -0,0 +1,9 @@
+﻿using DentalClinic.Shared.DTOs.Patients;
+
+namespace DentalClinic.Services.Auth;
+public class AuthResponse
+{
+    public string Token { get; set; }
+    public PatientDto User { get; set; }
+    public IEnumerable<string> Roles { get; set; }
+}
