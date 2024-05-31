0390b9a0572c00b80d83655aa21fa4f87a1bdcc5
Pavel-Kolenda <progap0002@gmail.com>
2024-05-31 10:47:47 +0300

    Add GetCurrentUser endpoint in AuthController

--- DentalClinic.API/Controllers/AuthController.cs
+++ DentalClinic.API/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using DentalClinic.Services.Auth;
 using DentalClinic.Shared.DTOs.Patients;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DentalClinic.API.Controllers;
@@ -30,5 +31,21 @@ public class AuthController : ControllerBase
         var response = await _authService.Login(patientLoginDto);
 
         return Ok(response);
+    }
+
+    [HttpGet]
+    [Authorize]
+    public async Task<ActionResult<AuthResponse>> GetCurrentUser()
+    {
+        var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == "Id");
+        if (userIdClaim == null)
+        {
+            return Unauthorized();
+        }
+
+        int userId = int.Parse(userIdClaim.Value);
+
+        AuthResponse authResponse = await _authService.GetCurrentUser(userId);
+        return authResponse;
     }
 }
