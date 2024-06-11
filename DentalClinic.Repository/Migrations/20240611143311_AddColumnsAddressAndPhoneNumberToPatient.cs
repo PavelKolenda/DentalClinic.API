ce7333c3eab358ed28945183d6f926fabddde6c3
Pavel-Kolenda <progap0002@gmail.com>
2024-06-11 17:38:39 +0300

    Create migration

---
+++ DentalClinic.Repository/Migrations/20240611143311_AddColumnsAddressAndPhoneNumberToPatient.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DentalClinic.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnsAddressAndPhoneNumberToPatient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Patients",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Patients",
+                type: "character varying(13)",
+                maxLength: 13,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Patients");
+        }
+    }
+}
