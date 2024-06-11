6115745aae41aae6bcd3c7f286ce83e45e91fdb9
Pavel-Kolenda <progap0002@gmail.com>
2024-06-11 17:37:52 +0300

    Add PhoneNumber and Address properties to Patient

--- DentalClinic.Models/Entities/Patient.cs
+++ DentalClinic.Models/Entities/Patient.cs
@@ -8,6 +8,8 @@ public class Patient
     public DateOnly BirthDate { get; set; }
     public string Email { get; set; }
     public string PasswordHash { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Address { get; set; }
     public ICollection<Appointment> Appointments { get; set; }
     public ICollection<Role> Roles { get; set; }
 }
