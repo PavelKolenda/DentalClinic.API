ee5bcc16a3a4b4ce7d645b2eb0fa02a84fd3c332
Pavel-Kolenda <progap0002@gmail.com>
2024-06-13 18:05:16 +0300

    Add NotificationsService

---
+++ DentalClinic.Services/NotificationsService.cs
@@ -0,0 +1,46 @@
+﻿using DentalClinic.Repository.Contracts;
+using DentalClinic.Repository.Contracts.Queries;
+using DentalClinic.Services.Contracts;
+using DentalClinic.Shared.DTOs.Notifications;
+using DentalClinic.Shared.Pagination;
+
+using Mapster;
+
+using Microsoft.AspNetCore.Http;
+
+namespace DentalClinic.Services;
+public class NotificationsService : INotificationsService
+{
+    private readonly INotificationsRepository _notificationsRepository;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public NotificationsService(INotificationsRepository notificationsRepository, IHttpContextAccessor httpContextAccessor)
+    {
+        _notificationsRepository = notificationsRepository;
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public PagedList<NotificationDto> GetPaged(QueryParameters query)
+    {
+        int patientId = GetPatientIdFromClaims();
+
+        var notifications = _notificationsRepository.GetPaged(patientId, query);
+
+        var notificationsDto = notifications.Items.Adapt<List<NotificationDto>>();
+
+        return new PagedList<NotificationDto>(notificationsDto, notifications.Page, notifications.PageSize, notifications.TotalCount);
+    }
+
+    public int GetPatientIdFromClaims()
+    {
+        var patientIdClaim = _httpContextAccessor.HttpContext.User.FindFirst("Id");
+
+        if (patientIdClaim == null)
+        {
+            throw new UnauthorizedAccessException("Patient isn't authorized");
+        }
+
+        int patientId = Convert.ToInt32(patientIdClaim.Value);
+        return patientId;
+    }
+}
