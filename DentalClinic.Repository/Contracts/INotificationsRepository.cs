1996ac7f1df1fbf1880b5651db290feffab62b0a
Pavel-Kolenda <progap0002@gmail.com>
2024-06-13 18:03:49 +0300

    Add NotificationRepository

---
+++ DentalClinic.Repository/Contracts/INotificationsRepository.cs
@@ -0,0 +1,10 @@
+﻿using DentalClinic.Models.Entities;
+using DentalClinic.Repository.Contracts.Queries;
+using DentalClinic.Shared.Pagination;
+
+namespace DentalClinic.Repository.Contracts;
+public interface INotificationsRepository
+{
+    Task<Notification> CreateAsync(Notification notification);
+    PagedList<Notification> GetPaged(int patientId, QueryParameters query);
+}
---
+++ DentalClinic.Repository/NotificationsRepository.cs
@@ -0,0 +1,32 @@
+﻿using DentalClinic.Models.Entities;
+using DentalClinic.Repository.Contracts;
+using DentalClinic.Repository.Contracts.Queries;
+using DentalClinic.Shared.Pagination;
+
+namespace DentalClinic.Repository;
+public class NotificationsRepository : INotificationsRepository
+{
+    private readonly ClinicDbContext _context;
+
+    public NotificationsRepository(ClinicDbContext context)
+    {
+        _context = context;
+    }
+
+    public PagedList<Notification> GetPaged(int patientId, QueryParameters query)
+    {
+        var dbQuery = _context.Notifications.AsQueryable()
+            .Where(x => x.PatientId == patientId)
+            .OrderBy(x => x.SandedAt);
+
+        return PagedListExtensions<Notification>.Create(dbQuery, query.Page, query.PageSize);
+    }
+
+    public async Task<Notification> CreateAsync(Notification notification)
+    {
+        await _context.Notifications.AddAsync(notification);
+        await _context.SaveChangesAsync();
+
+        return notification;
+    }
+}
