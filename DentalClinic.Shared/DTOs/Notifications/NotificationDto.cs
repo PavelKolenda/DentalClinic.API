a1f7be18b06feb1b959c7461261c3cc1e9ad6f22
Pavel-Kolenda <progap0002@gmail.com>
2024-06-13 18:04:28 +0300

    Add NotificationDto and NotificationCreateDto

---
+++ DentalClinic.Shared/DTOs/Notifications/NotificationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DentalClinic.Shared.DTOs.Notifications;
+public class NotificationDto
+{
+    public int Id { get; set; }
+    public string Article { get; set; }
+    public string Text { get; set; }
+    public DateTime SandedAt { get; set; }
+}
