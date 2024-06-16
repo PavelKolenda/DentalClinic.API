3aa3dca8fb946f2dc9a50e44c8bee6d45fae7f35
Pavel-Kolenda <progap0002@gmail.com>
2024-06-16 13:58:30 +0300

    Add NewsRepository

---
+++ DentalClinic.Repository/Contracts/INewsRepository.cs
@@ -0,0 +1,15 @@
+﻿using DentalClinic.Models.Entities;
+using DentalClinic.Repository.Contracts.Queries;
+using DentalClinic.Shared.Pagination;
+
+namespace DentalClinic.Repository.Contracts
+{
+    public interface INewsRepository
+    {
+        Task<News> CreateAsync(News news);
+        Task DeleteAsync(int id);
+        Task<News> GetById(int id);
+        PagedList<News> GetPaged(QueryParameters query);
+        Task UpdateAsync(int id, News news);
+    }
+}
