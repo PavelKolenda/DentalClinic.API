2177eece5fe1f1d2adbf8fc5f2be81e01ec2b5cf
Pavel-Kolenda <progap0002@gmail.com>
2024-06-16 11:15:32 +0300

    Add SendNotificationsAboutAppointmentInTwoHoursJob to jobs

--- DentalClinic.API/Extensions/ServiceExtensions.cs
+++ DentalClinic.API/Extensions/ServiceExtensions.cs
@@ -3,6 +3,7 @@
 using DentalClinic.API.Extensions.ErrorHandling;
 using DentalClinic.Repository;
 using DentalClinic.Services.Jobs;
+using DentalClinic.Services.Jobs.Notifications;
 using DentalClinic.Services.Mappings;
 
 using FluentValidation;
@@ -49,6 +50,21 @@ public static class ServiceExtensions
             //    .ForJob(createAppointmentsForMonth)
             //    .WithIdentity("CreateAppointmentsForMonth-trigger")
             //    .WithSimpleSchedule(schedule => schedule.WithRepeatCount(0)));
+
+            var sendNotificationsAboutAppointmentInTwoHours = new JobKey("SendNotificationsAboutAppointmentInTwoHoursJob");
+
+            opt.AddJob<SendNotificationsAboutAppointmentInTwoHoursJob>(opt =>
+            {
+                opt.WithIdentity(sendNotificationsAboutAppointmentInTwoHours);
+            });
+
+            opt.AddTrigger(opt =>
+            {
+                opt
+                .ForJob(sendNotificationsAboutAppointmentInTwoHours)
+                .WithIdentity("SendNotificationsAboutAppointmentInTwoHoursJob")
+                .WithCronSchedule("0 0/30 * 1/1 * ? *");
+            });
         });
     }
 
