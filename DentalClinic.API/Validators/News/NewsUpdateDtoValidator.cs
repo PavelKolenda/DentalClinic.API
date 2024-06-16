aef38da257808b8bfee5c117939d230611e9c2f4
Pavel-Kolenda <progap0002@gmail.com>
2024-06-16 13:59:25 +0300

    Add NewsValidators

---
+++ DentalClinic.API/Validators/News/NewsUpdateDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using DentalClinic.Shared.DTOs.News;
+
+using FluentValidation;
+
+namespace DentalClinic.API.Validators.News;
+
+public class NewsUpdateDtoValidator : AbstractValidator<NewsUpdateDto>
+{
+    public NewsUpdateDtoValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty()
+            .MaximumLength(500);
+
+        RuleFor(x => x.Text)
+            .NotEmpty()
+            .MaximumLength(10000);
+
+        RuleFor(x => x.CreatedAt)
+            .NotEmpty();
+    }
+}
+
