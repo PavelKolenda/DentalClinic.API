03da98db2415f1cdd0ccc3fab746b294a526a8f9
Pavel-Kolenda <progap0002@gmail.com>
2024-06-17 21:58:48 +0300

    Add DentistDtoAsUser

---
+++ DentalClinic.Shared/DTOs/Dentists/DentistDtoAsUser.cs
@@ -0,0 +1,9 @@
+﻿namespace DentalClinic.Shared.DTOs.Dentists;
+
+public class DentistDtoAsUser : DentistDtoBase
+{
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateOnly BirthDate { get; set; }
+}
