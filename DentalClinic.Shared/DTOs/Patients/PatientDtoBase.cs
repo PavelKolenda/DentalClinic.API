3b38eabbf7c8a8d70b117062534aead1fe023c73
Pavel-Kolenda <progap0002@gmail.com>
2024-06-18 23:19:14 +0300

    Update PatientDtoBase

--- DentalClinic.Shared/DTOs/Patients/PatientDtoBase.cs
+++ DentalClinic.Shared/DTOs/Patients/PatientDtoBase.cs
@@ -6,7 +6,7 @@ public abstract class PatientDtoBase
     public string Surname { get; set; }
     public string? Patronymic { get; set; }
     public string Email { get; set; }
-    public string Password { get; set; }
+    public string? Password { get; set; }
     public DateOnly BirthDate { get; set; }
     public string PhoneNumber { get; set; }
     public string Address { get; set; }
